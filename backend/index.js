c1c37d905f57261641f34e79b06c6f430ea137bb
Pratyush-48 <pratyushprasad16.com>
2026-05-30 12:21:23 +0530

    Update static file serving path and clean up environment variables

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ app.use("/api/v1/user",userRoute);
 app.use("/api/v1/message",messageRoute);
  
 
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+app.use(express.static(path.join(_dirname, "frontend", "dist")));
 app.get('*', (_, res) => {
     res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
 });
